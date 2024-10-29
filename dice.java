64bff9b8b1dedcb18d18f260e4dc87176607b6c0
Flace333 <magnusfehrmann@gmail.com>
2024-10-29 23:43:53 +0100

    Made some classes so we have some instances. Made demo game class (Waiting for Mr. Ludvig to make his big move on Player and account) Made dice class.

---
+++ dice.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+class Dice {
+    int die1;
+    int die2;
+    int dieSum;
+
+    public static int RandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
+    }
+
+    public int roll() {
+        die1 = RandomNumber(1, 6);
+        die2 = RandomNumber(1, 6);
+        dieSum = die1 + die2;
+        return dieSum;
+    }
+
+}
