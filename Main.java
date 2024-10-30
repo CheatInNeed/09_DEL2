ec19439431a6ab16597615502275304e27d838a1
CheatInNeed <peterroland02@gmail.com>
2024-10-30 16:27:15 +0100

    added main class

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(java.util.Locale.ENGLISH);
+
+        String userInput = scanner.nextLine();
+
+        Player[] players = new Player[2];
+        GameBoard board = new GameBoard();
+        Dice dice = new Dice();
+
+    while (!isWinning()){
+        System.out.println("Play the next round by typing 'r'");
+        if (userInput.equals("r")){
+        for (var n : players){
+
+            int diceSum = dice.roll();
+            int boardValue = board.getValue(diceSum);
+            
+            if (boardValue > 0){
+            System.out.println("Player " + n + " rolled: " + diceSum + " and goes to the field. " + boardValue + " amount of money has been added to your account.");
+            } else {System.out.println("Player " + n + " rolled: " + diceSum + " and goes to the field. " + boardValue + " amount of money has been withdrawed from your account.");}
+        } 
+    } else {}
+
+    }
+
+
+
+
+
+
+    }
+}
